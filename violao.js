1d93afd63cec627dc0e2884dd8b2df27081367c1
Victor <victor.alves.sales@hotmail.com>
2023-02-22 11:18:03 -0300

    violao

--- violao.js
+++ violao.js
@@ -1,3 +1,4 @@
+
 for(x=0; x<document.querySelectorAll(".drum").length+1; x++){
 
 document.querySelectorAll("button")[x].addEventListener("click", handleClick)
@@ -24,34 +25,29 @@ document.addEventListener("keydown", function(event){
 function makeSound(key){
     switch (key) {
         case "w":
-        var tom1 = new Audio ("sounds/tom-1.mp3")
+        var tom1 = new Audio ("sounds/violao/corda1.mp3")
         tom1.play();
         break;
     case "a":
-        var tom2 = new Audio ("sounds/tom-2.mp3")
+        var tom2 = new Audio ("sounds/violao/corda2.mp3")
         tom2.play();
         break;
     case "s":
-        var tom3 = new Audio ("sounds/tom-3.mp3")
+        var tom3 = new Audio ("sounds/violao/corda3.mp3")
         tom3.play();
         break;
     case "d":
-        var tom4 = new Audio ("sounds/tom-4.mp3")
+        var tom4 = new Audio ("sounds/violao/corda4.mp3")
         tom4.play();
         break;
     case "j":
-        var snare = new Audio ("sounds/snare.mp3")
+        var snare = new Audio ("sounds/violao/corda5.mp3")
         snare.play();
         break;
     case "k":
-        var crash = new Audio ("sounds/crash.mp3")
+        var crash = new Audio ("sounds/violao/corda6.wav")
         crash.play();
         break;
-    case "l":
-        var kick = new Audio ("sounds/kick-bass.mp3")
-        kick.play();
-        break;
-
     default:
         console.log("erro");
     }
