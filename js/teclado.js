6634d6d76305152f682a6485dfc32705fc5ebe68
Victor <victor.alves.sales@hotmail.com>
2023-02-28 19:40:31 -0300

    folder

--- js/teclado.js
+++ js/teclado.js
@@ -25,27 +25,27 @@ document.addEventListener("keydown", function(event){
 function makeSound(key){
     switch (key) {
         case "w":
-        var tom1 = new Audio ("sounds/violao/corda1.mp3")
+        var tom1 = new Audio ("sounds/teclado/corda1.mp3")
         tom1.play();
         break;
     case "a":
-        var tom2 = new Audio ("sounds/violao/corda2.mp3")
+        var tom2 = new Audio ("sounds/teclado/corda2.mp3")
         tom2.play();
         break;
     case "s":
-        var tom3 = new Audio ("sounds/violao/corda3.mp3")
+        var tom3 = new Audio ("sounds/teclado/corda3.mp3")
         tom3.play();
         break;
     case "d":
-        var tom4 = new Audio ("sounds/violao/corda4.mp3")
+        var tom4 = new Audio ("sounds/teclado/corda4.mp3")
         tom4.play();
         break;
     case "j":
-        var snare = new Audio ("sounds/violao/corda5.mp3")
+        var snare = new Audio ("sounds/teclado/corda5.mp3")
         snare.play();
         break;
     case "k":
-        var crash = new Audio ("sounds/violao/corda6.wav")
+        var crash = new Audio ("sounds/teclado/corda6.wav")
         crash.play();
         break;
     default:
